c796f958fd4f201e878fa016a46ab0d389778780
mutoni23 <mutoniv5@gmail.com>
2021-01-23 18:01:50 +0200

    added tests.py

--- awwards/tests.py
+++ awwards/tests.py
@@ -15,7 +15,6 @@ class RatingTestClass(TestCase):
     def setUp(self):
         self.user1 = User(username="mutoni23")
         self.user1.save()
-        
         self.project1 = Project(title="delani-studio",description="delani-studio projo",link="mutoni23.github.io/delani-studio/",image="delani-studio",user=self.user1)
         self.project1.save()
         self.new_rating = Rating(design=9,usability=9,content=9,project=self.project1,user=self.user1)
