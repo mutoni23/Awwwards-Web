19933e0990bb92d88417a68c35b1bf9ef5d17a88
mutoni23 <mutoniv5@gmail.com>
2021-01-23 18:29:08 +0200

    serializers.py

--- awwards/serializer.py
+++ awwards/serializer.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from .models import Project
 from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = User
         fields = ("username","email")
