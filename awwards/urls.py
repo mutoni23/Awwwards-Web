2e5454ec00b709fc9617cd9cdb0861f405e5a186
mutoni23 <mutoniv5@gmail.com>
2021-01-23 18:08:28 +0200

    added urls.py

--- awwards/urls.py
+++ awwards/urls.py
@@ -4,7 +4,6 @@ from . import views
 urlpatterns = [
     re_path(r'^$',views.Index_view,name="index_view"),
     re_path(r'^upload/$',views.Upload_Project,name="upload_project"),
-    
     re_path(r'^rating/(?P<pk>\d+)$',views.RateProject,name="rate_project"),
     re_path(r'^myprofile/',views.User_Profile,name="my_profile"),
     re_path(r'^devcenter/',views.Dev_center,name="dev_center"),
