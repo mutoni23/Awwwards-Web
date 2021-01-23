4c40d71c70e19687f25b81033623db046eb8cc3e
mutoni23 <mutoniv5@gmail.com>
2021-01-23 18:00:47 +0200

    added tests.py

--- awwards/tests.py
+++ awwards/tests.py
@@ -5,7 +5,6 @@ from users.models import User,Profile
 class ProjectTestClass(TestCase):
     def setUp(self):
         self.user1 = User(username="mutoni23")
-        
         self.user1.save()
         self.project1 = Project(title="delani-studio",description="delani-studio projo",link="mutoni23.github.io/delani-studio/",image="delani-studio",user=self.user1)
 
